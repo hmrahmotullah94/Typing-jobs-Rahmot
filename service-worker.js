7b84150d1faf580108b4aa761fbbef65b46dc17b
hmrahmotullah94 <hm.rahmotullah94@gmail.com>
2026-07-29 14:25:00 +0600

    Refactor service worker event listeners

--- service-worker.js
+++ service-worker.js
@@ -13,54 +13,53 @@ const FILES = [
 
 self.addEventListener("install", event => {
 
-    event.waitUntil(
-        caches.open(CACHE)
-        .then(cache => cache.addAll(FILES))
-    );
+event.waitUntil(
+caches.open(CACHE)
+.then(cache=>cache.addAll(FILES))
+);
 
-    self.skipWaiting();
-
-});
-
-
-self.addEventListener("activate", event => {
-
-    event.waitUntil(
-
-        caches.keys().then(keys => {
-
-            return Promise.all(
-
-                keys.map(key => {
-
-                    if(key !== CACHE){
-                        return caches.delete(key);
-                    }
-
-                })
-
-            );
-
-        })
-
-    );
-
-    self.clients.claim();
+self.skipWaiting();
 
 });
 
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", event=>{
 
-    event.respondWith(
+event.waitUntil(
 
-        caches.match(event.request)
-        .then(response => {
+caches.keys().then(keys=>
 
-            return response || fetch(event.request);
+Promise.all(
 
-        })
+keys.map(key=>{
 
-    );
+if(key!==CACHE)
+return caches.delete(key);
+
+})
+
+)
+
+)
+
+);
+
+self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
 
 });
